29af53585a727b2244d25e2d654b3cb28bc8f3a7
SudipR <lycansu@gmail.com>
2023-03-09 20:15:15 +0545

    Number format for sms fixed

--- app/Webifi/Services/SMSService.php
+++ app/Webifi/Services/SMSService.php
@@ -20,6 +20,7 @@ class SMSService
      */
     public function sendSMS($receiverNumber = "", $message = "")
     {
+        $receiverNumber = $this->convertNumber($receiverNumber);
         try {
   
             $account_sid = getenv("TWILIO_SID");
@@ -35,6 +36,26 @@ class SMSService
   
         } catch (\Exception $e) {
             return ['error' => $e->getMessage()];
+        }
+    }
+
+    /**
+     * Convert to international number if local provided
+     * 
+     * @param $number
+     * @return String
+     */
+    private function convertNumber($number)
+    {
+        // Check if the phone number starts with '0'
+        if (substr($number, 0, 1) === '0') {
+            // Add the country code prefix
+            $countryCode = '+44'; // Country code for UK
+            $phoneNumberWithCode = $countryCode . substr($number, 1);
+
+            return $phoneNumberWithCode; 
+        } else {
+            return $number;
         }
     }
 
